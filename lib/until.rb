8964047260adad69dd9d181ddfcb8d2448a7e0f4
Mark Harvey <maharvey2@crimson.ua.edu>
2019-04-13 17:48:35 +0000

    Done.

--- lib/until.rb
+++ lib/until.rb
@@ -1,6 +1,9 @@
 def using_until
   levitation_force = 6
   #your code here
-    
+  #I will never understand why poeple like Harry potter...
+until levitation_force ==10
+  puts "Wingardium Leviosa"
+  levitation_force += 1
+  end
 end
-
